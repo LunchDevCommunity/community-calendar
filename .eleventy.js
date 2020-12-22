076746e1234194d031b2d5c3656f903db6cdad4d
Matt Sutkowski <msutkowski@gmail.com>
2020-12-21 19:11:02 -0800

    Add todays events to the upcoming list

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,24 @@
 const markdownIt = require('markdown-it');
 const emoji = require('markdown-it-emoji');
 
-const { isAfter, isBefore } = require('date-fns');
+const { isAfter, isBefore, isToday } = require('date-fns');
 
-module.exports = (eleventyConfig) => {
-  eleventyConfig.addCollection('events', (collectionApi) => {
+module.exports = (eleventyConfiguration) => {
+  eleventyConfiguration.addCollection('events', (collectionApi) => {
     return collectionApi.getFilteredByGlob('./src/schedule/*.md');
   });
-  eleventyConfig.addCollection('pastEvents', (collectionApi) => {
+  eleventyConfiguration.addCollection('pastEvents', (collectionApi) => {
     return collectionApi
       .getFilteredByGlob('./src/schedule/*.md')
       .filter((event) => isBefore(new Date(event.data.date), new Date()));
   });
-  eleventyConfig.addCollection('upcomingEvents', (collectionApi) => {
+  eleventyConfiguration.addCollection('upcomingEvents', (collectionApi) => {
+    // We include `isToday` assuming that a daily build job is ran at 12:01am,
+    // and we're okay with a daily event potentially showing even if it's in the past
+    // _(until 12:00:59am of the next day)_
     return collectionApi
       .getFilteredByGlob('./src/schedule/*.md')
-      .filter((event) => isAfter(new Date(event.data.date), new Date()));
+      .filter((event) => isAfter(new Date(event.data.date), new Date()) || isToday(new Date(event.data.date)));
   });
 
   let markdown = markdownIt({
