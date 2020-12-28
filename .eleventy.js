4928e3c1f2cce6888f16b76e3660a28dcfadd674
Michael Chan <mijoch@gmail.com>
2020-12-28 10:17:07 -0800

    reformat eleventy.js with prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,34 +1,34 @@
 const markdownIt = require('markdown-it');
 const emoji = require('markdown-it-emoji');
 
-const {isAfter, isBefore} = require('date-fns');
+const { isAfter, isBefore } = require('date-fns');
 
 module.exports = (eleventyConfiguration) => {
-	eleventyConfiguration.addCollection('events', (collectionApi) => {
-		return collectionApi.getFilteredByGlob('./src/schedule/*.md');
-	});
-	eleventyConfiguration.addCollection('pastEvents', (collectionApi) => {
-		return collectionApi
-			.getFilteredByGlob('./src/schedule/*.md')
-			.filter(event => isBefore(new Date(event.data.date), new Date()));
-	});
-	eleventyConfiguration.addCollection('upcomingEvents', (collectionApi) => {
-		return collectionApi
-			.getFilteredByGlob('./src/schedule/*.md')
-			.filter(event => isAfter(new Date(event.data.date), new Date()));
-	});
+  eleventyConfiguration.addCollection('events', (collectionApi) => {
+    return collectionApi.getFilteredByGlob('./src/schedule/*.md');
+  });
+  eleventyConfiguration.addCollection('pastEvents', (collectionApi) => {
+    return collectionApi
+      .getFilteredByGlob('./src/schedule/*.md')
+      .filter((event) => isBefore(new Date(event.data.date), new Date()));
+  });
+  eleventyConfiguration.addCollection('upcomingEvents', (collectionApi) => {
+    return collectionApi
+      .getFilteredByGlob('./src/schedule/*.md')
+      .filter((event) => isAfter(new Date(event.data.date), new Date()));
+  });
 
-	let markdown = markdownIt({
-		typographer: true
-	});
+  let markdown = markdownIt({
+    typographer: true,
+  });
 
-	markdown.use(emoji);
+  markdown.use(emoji);
 
-	eleventyConfiguration.setLibrary('md', markdown);
+  eleventyConfiguration.setLibrary('md', markdown);
 
-	return {
-		dir: {
-			input: './src/'
-		}
-	};
+  return {
+    dir: {
+      input: './src/',
+    },
+  };
 };
