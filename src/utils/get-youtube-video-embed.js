c3bcbbce8a6e112d65000ac4cf62a576df31ae1b
Michael Chan <mijoch@gmail.com>
2021-01-20 12:23:24 -0800

    fixup! add youtube-video shortcode

--- src/utils/get-youtube-video-embed.js
+++ src/utils/get-youtube-video-embed.js
@@ -1,3 +1,8 @@
+/**
+ * Generates the markup for a responsive YouTube video embed.
+ * @param {string} url URL of the YouTube upload
+ * @returns {string} YouTube embed markup
+ */
 module.exports = function getYouTubeVideoEmbed(url) {
 	return `<div data-responsive-youtube--container>
     <iframe src="//www.youtube.com/embed/${getVideoIdFromYouTubeURL(url)}" frameborder="0" allowfullscreen></iframe>
