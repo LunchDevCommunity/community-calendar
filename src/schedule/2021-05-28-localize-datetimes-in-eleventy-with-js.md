9e2c2956805af477c46e3d6c0f69296251e4eeb1
Michael Chan <mijoch@gmail.com>
2021-05-26 13:18:30 -0700

    fixup! add speaker avatars to event page

--- src/schedule/2021-05-28-localize-datetimes-in-eleventy-with-js.md
+++ src/schedule/2021-05-28-localize-datetimes-in-eleventy-with-js.md
@@ -4,7 +4,6 @@ type: Mob Job
 date: 2021-05-28 17:00:00
 speakers:
   - Michael Chan
-  - Ben Myers
 ---
 
 GitHub issue: https://github.com/LunchDevCommunity/community-calendar/issues/71
--- src/utils/person.js
+++ src/utils/person.js
@@ -1,9 +1,9 @@
 module.exports.getAvatarUrl = function (user) {
-	if (user.avatar_url) {
-		return user.avatar_url;
+	if (user?.avatar_url) {
+		return user?.avatar_url;
 	}
 
-	if (user.github) {
+	if (user?.github) {
 		return `https://avatars.githubusercontent.com/${user.github}`;
 	}
 
